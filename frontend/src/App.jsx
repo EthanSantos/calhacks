92db685bb57a4b1b332713e9117f1e259c30e6f8
EthanSantos <ethanangelo61@gmail.com>
2024-06-22 11:05:20 -0700

    page routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Hello this is the home page
+    </div>
+  )
+}
+
+export default Home
