62b7295dc1c22497688d08efb0056125852cd116
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-06-23 06:53:39 -0700

    fixing merge conflicts

--- frontend/src/pages/input/UserInput.jsx
+++ frontend/src/pages/input/UserInput.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { getCompletion } from '../../helper/geminiAiService';
 import Contract from '../contract/Contract';
-import UrComponent from './UrComponent.jsx';
-import Loading from '../Loading.jsx';
+import Loading from '../loading/Loading';
 
 const UserInput = () => {
     const [page, setPage] = useState(0);
@@ -63,7 +62,8 @@ const UserInput = () => {
 
     return (
         <>
-            {loading && <p>Loading...</p>}
+            {loading && <Loading />}
+            {/* {loading && <p>Loading...</p>} */}
             {!loading && contractData.length === 0 && (
                 <div className="max-w-lg mx-auto mt-10 p-6 bg-white shadow-md rounded-md">
                     <form onSubmit={page === 2 ? handleSubmit : handleNext}>
@@ -164,152 +164,6 @@ const UserInput = () => {
                 <Contract headers={sections} contractData={contractData} />
             )}
         </>
-        <div className="flex flex-row justify-center items-center h-screen">
-            <UrComponent />
-            <div className="relative flex justify-left items-center">
-                <img src={'./src/pics/anim1.png'} alt="Anim1" className="anim1 h-[650px] w-auto ml-[150px]"/>
-                <img src={'./src/pics/anim2.png'} alt="Anim2" className="anim2 h-[450px] w-auto"/>
-            </div>
-            <div className='flex justify-center align-center h-full w-full ml-[250px] p-[150px] bg-gray rounded-[30px]'>
-                <form onSubmit={page === 2 ? handleSubmit : handleNext} className='flex flex-col justify-center align-center max-w-auto'>
-                {/* {loading && <p>Loading...</p>} */}
-                {loading && <Loading />}
-
-                {!loading && content && 
-                
-                Array.isArray(content) && content.map((para, index) => (
-                    <div key={index} >
-                        <p key={index}>{para}</p><br/>
-                    </div>
-                ))}
-                    {page === 0 && (
-                        <div>
-                            <h1 className="flex text-5xl font-bold mb-4 text-blue text-shadow-custom justify-center align-center">Let's get to know you!</h1>
-                            <h1 className="flex justify-center align-center text-3xl mb-4 text-blue">Part A: Bio</h1>
-                            <div className="mb-4">
-                                <label htmlFor="name" className="block text-white font-bold mb-2">Name</label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="profession" className="block text-white font-bold mb-2">Profession</label>
-                                <input
-                                    type="text"
-                                    id="profession"
-                                    value={profession}
-                                    onChange={(e) => setProfession(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="services" className="block text-white font-bold mb-2">Services</label>
-                                <textarea
-                                    id="services"
-                                    value={services}
-                                    onChange={(e) => setServices(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                        </div>
-                    )}
-                    {page === 1 && (
-                        <div>
-                            <h1 className="flex text-5xl font-bold mb-4 text-blue text-shadow-custom justify-center align-center">Let's get to know you!</h1>
-                            <h1 className="flex justify-center align-center text-3xl mb-4 text-blue">Part B: Timeline</h1>
-                            <div className="mb-4">
-                                <label htmlFor="startDate" className="block text-white font-bold mb-4">Start Date</label>
-                                <input
-                                    type="date"
-                                    id="startDate"
-                                    value={startDate}
-                                    onChange={(e) => setStartDate(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="endDate" className="block text-white font-bold mb-4">End Date</label>
-                                <input
-                                    type="date"
-                                    id="endDate"
-                                    value={endDate}
-                                    onChange={(e) => setEndDate(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="checkIns" className="block text-white font-bold mb-4">Check-Ins</label>
-                                <input
-                                    type="text"
-                                    id="checkIns"
-                                    value={checkIns}
-                                    onChange={(e) => setCheckIns(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                        </div>
-                    )}
-                    {page === 2 && (
-                        <div>
-                            <h1 className="flex text-5xl font-bold mb-4 text-blue text-shadow-custom justify-center align-center">Let's get to know you!</h1>
-                            <h1 className="flex justify-center align-center text-3xl mb-4 text-blue">Part C: Experience and Payment</h1>
-                            <div className="mb-4">
-                                <label htmlFor="yearsOfExp" className="block text-white font-bold mb-4">Years of Experience</label>
-                                <input
-                                    type="number"
-                                    id="yearsOfExp"
-                                    value={yearsOfExp}
-                                    onChange={(e) => setYearsOfExp(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="payment" className="block text-white font-bold mb-4">Payment</label>
-                                <input
-                                    type="text"
-                                    id="payment"
-                                    value={payment}
-                                    onChange={(e) => setPayment(e.target.value)}
-                                    required
-                                    className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                            </div>
-                        </div>
-                    )}
-                    {page < 2 && (
-                        <div className="relative group">
-                            <div className="absolute -inset-0.5 bg-gradient-to-r from-red to-blue rounded-lg blur opacity-75"></div>
-                            <button onClick={handleNext} className="relative w-full bg-black text-2xl text-white font-bold py-4 rounded-md
-                            group-hover:bg-blue group-hover:text-black transition-colors duration-300 ease-in-out">
-                                Next
-                            </button>
-                        </div>
-                        
-                    )}
-                    {page === 2 && (
-                        <div className="relative group">
-                            <div className="absolute -inset-0.5 bg-gradient-to-r from-red to-blue rounded-lg blur opacity-75"></div>
-                            <button type="submit" className="relative w-full bg-black text-white text-2xl font-bold py-4 rounded-md
-                            group-hover:bg-blue group-hover:text-black transition-colors duration-300 ease-in-out">
-                            Submit
-                        </button>
-                        </div>
-                    )}
-                </form>
-                
-            </div>
-        </div>
     );
 };
 
