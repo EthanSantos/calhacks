e231c964225f88a32db7c75d7da03ef62a1342dc
Aurelisa Sindhunirmala <aurelisajuans@gmail.com>
2024-06-22 17:04:47 -0700

    added learn more

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
       <p className="font-satoshi text-[25px] text-center text-yellow">
         Your go-to platform for <span class="italic">generating</span> and <span class="italic">interpreting</span> contracts.
       </p>
+      <img src={'./src/pics/homeTri.png'} alt="Bottom" className="h-[65%] absolute bottom-0 right-0"/>
+      <p className="font-satoshi text-lg text-gray-500 absolute bottom-6">Learn more</p>
     </div>
   )
 }
